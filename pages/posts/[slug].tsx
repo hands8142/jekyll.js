2d69a6a89e947c70cada614a9ffba53f1eebf560
hands8142 <hands8142@naver.com>
2021-01-06 11:40:05 +0900

    add disqus

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -7,7 +7,7 @@ import { getPostBySlug, getAllPosts } from "@util";
 import Header from "@includes/header";
 import PostHeader from "@includes/post_header";
 
-export default function PostTemplate({ post, config }) {
+export default function PostTemplate({ post, config, slug }) {
   return (
     <>
       <Analytics config={config} />
@@ -18,6 +18,32 @@ export default function PostTemplate({ post, config }) {
           <div className="post-container">
             <ReactMarkdown children={post.content} renderers={renderers} />
           </div>
+          {config.disqus_shortname ? (
+            <>
+              <div id="disqus_thread" style={{ marginTop: "25px" }}></div>
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `var disqus_config = function () {
+            this.page.url = "${config.baseUrl}/posts/${slug}/";
+            this.page.identifier = "${config.baseUrl}/posts/${slug}/";
+          };
+          (function () {
+            var d = document,
+              s = d.createElement("script");
+            s.src = "https://${config.disqus_shortname}.disqus.com/embed.js";
+            s.setAttribute("data-timestamp", +new Date());
+            (d.head || d.body).appendChild(s);
+          })();`,
+                }}
+              />
+              <noscript>
+                Please enable JavaScript to view the
+                <a href="https://disqus.com/?ref_noscript" rel="nofollow">
+                  comments powered by Disqus.
+                </a>
+              </noscript>
+            </>
+          ) : null}
         </div>
       </main>
     </>
@@ -51,11 +77,13 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
 export const getStaticProps: GetStaticProps = async context => {
   const config = yaml.safeLoad(fs.readFileSync("./config.yml", "utf8"), "utf8");
-  const post = await getPostBySlug(context.params.slug);
+  const slug = context.params.slug;
+  const post = await getPostBySlug(slug);
   return {
     props: {
       post,
       config,
+      slug,
     },
   };
 };
